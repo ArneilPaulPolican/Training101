6f98cdd02a0858ddcb916abe7317ba0532a37265
HAR-HAR <HAR-HAR@HAR-HAR-PC>
2017-06-30 11:37:45 +0800

    First Commit

---
+++ training102/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="training102.MvcApplication" Language="C#" %>
---
+++ training102/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(training102.Startup))]
+namespace training102
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
